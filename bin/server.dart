f7007ad9748af73b51cf8cf87735187d6d80dd23
RAHUL SHARMA <95410264+rahulsharmadev-community@users.noreply.github.com>
2022-05-23 19:27:03 +0530

    set data

--- bin/server.dart
+++ bin/server.dart
@@ -10,14 +10,19 @@ import 'api.dart';
 Future<void> main(List<String> args) async {
   //// initialing api
   final api = Api();
-  // Use any available host or container IP (usually `0.0.0.0`).
-  final ip = InternetAddress.anyIPv4;
+
+  // localhost
+  // const _hostname = InternetAddress.anyIPv4;
+
+  // For Google Cloud Run, set _hostname to '0.0.0.0'
+  const _hostname = '0.0.0.0';
+
   // Configure a pipeline that logs requests.
   final handler =
       Pipeline().addMiddleware(logRequests()).addHandler(api.router);
   // For running in containers, we respect the PORT environment variable.
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
 
-  final server = await serve(handler, ip, port);
+  final server = await serve(handler, _hostname, port);
   print('Server listening on port ${server.port}');
 }
